97c5743ca1ba81c377ca7bd4d60d148ed57a4e27
Shaswat Satyam <70892928+shaswat-satyam@users.noreply.github.com>
2026-01-18 16:34:37 +0530

    Created a basic table with format

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,26 @@ import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 
 function App() { const [count, setCount] = useState(0)
+  const products = [
+	  {"code":1, "name":"Hello", "category":"game", "quantity":5},
+	  {"code":1, "name":"Hello", "category":"game", "quantity":5},
+	  {"code":1, "name":"Hello", "category":"game", "quantity":5},
+  ]
 
+  const columns = [
+	  {"field":"code","header":"Code"},
+	  {"field":"name","header":"Name"},
+	  {"field":"category","header":"Category"},
+	  {"field":"quantity","header":"Quantity"},
+  ];
   return (
     <>
-       <p> The count is {count}</p>
-	<Button onClick = { () => setCount((count) => count + 1)}>
-	Hello There!
-        </Button>
-
 	<div>
-	<DataTable>
-	  <Column field="code" Header="Code"></Column>
-	</DataTable>
+	    <DataTable value={products} stripedRows tableStyle = {{minWidth: '50rem'}}>
+	    {columns.map((col, i) => (
+		    <Column key={col.field} field={col.field} header = {col.header}/>
+	    ))}
+	    </DataTable>
 	</div>
     </>
   )
